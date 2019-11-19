2ac36d849fc19841f80dbd4acd163f56e866bd9e
Ben Ray <benray887@gmail.com>
2019-11-19 15:32:03 -0800

    removed trunq

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Layout from '../components/layout'
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
-import trunq from 'trunq'
 
 class IndexPage extends React.Component {
   constructor(props) {
