66e79ce2cef586203c42d073753c7c8eb63bcfc0
Ben Ray <benray887@gmail.com>
2019-11-21 07:58:39 -0800

    working og images still for twitter

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,14 +46,11 @@ const Layout = ({ children, location }) => {
               { property: "og:title", content: 'TrunQ'},
               { property: "og:url", content: 'https://trunq.io/'},
               { property: "og:description", content: 'Easy to Implement GraphQL Solution!'},
-              { property: "og:image", content: "https://github.com/oslabs-beta/TrunQ/raw/master/demo/assets/trunqsocialbanner.png"} ,
-              { property: "twitter:card", content: "https://github.com/oslabs-beta/TrunQ/raw/master/demo/assets/trunqsocialbanner.png"}
-              { property: "twitter:image", content: "https://github.com/oslabs-beta/TrunQ/raw/master/demo/assets/trunqsocialbanner.png"} ,
+              { property: "og:image", content: "https://github.com/oslabs-beta/TrunQ/raw/master/demo/assets/trunqsocialbanner.png"},
+              { property: "twitter:card", content: "https://github.com/oslabs-beta/TrunQ/raw/master/demo/assets/trunqsocialbanner.png"},
+              { property: "twitter:image", content: "https://github.com/oslabs-beta/TrunQ/raw/master/demo/assets/trunqsocialbanner.png"},
               { property: "twitter:title", content: 'TrunQ'},
-              { property: "twitter:description", content: 'Easy to Implement GraphQL Solution!'},
-
-
-
+              { property: "twitter:description", content: 'Easy to Implement GraphQL Solution!'}
             ]}
           >
             <html lang="en" />
