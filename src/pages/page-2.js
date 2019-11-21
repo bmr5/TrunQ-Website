f80962f03e169f2f4cf36174ce4301085a0dbfb2
Ben Ray <benray887@gmail.com>
2019-11-21 07:20:40 -0800

    working og image

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import socialbanner from '../images/trunqsocialbanner.png'
 
 import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>Welcome to page 2! This is where we store our social banner</p>
+    <img src={socialbanner}></img>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
